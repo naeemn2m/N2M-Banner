328c0b7c9c0b45dabc8d166f44f151c7e767c2d0
naeemn2m <115134545+naeemn2m@users.noreply.github.com>
2022-10-28 12:34:28 +0600

    Add files via upload
    pkg update && pkg upgrade
    
    pkg install git -y
    
    git clone https://github.com/naeemn2m/N2M-Banner.git
    
    cd N2M-Banner
    
    chmod +x *
    
    bash njn2m.sh
    
    exit

---
+++ njn2m.sh
@@ -0,0 +1,38 @@
+#/data/data/com.termux/files/usr/etc
+clear
+echo
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        ✪Created by ✪NAEEM KHAN       |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        ♥INSTAGRAM:♥ naeemn2m        |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        ♦FACEBOOK:♦m.me/mdnaeem.n2m  |"
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo
+echo -e '\033[1;96m' "Facebook: https://m.Facebook.com/mdnaeem.n2m"
+echo -e '\033[1;92m'
+echo "Installing.................."
+echo
+apt install python -y
+pip install lolcat
+pkg install figlet
+pkg install mpv -y
+echo
+rm /data/data/com.termux/files/usr/etc/bash.bashrc
+rm /data/data/com.termux/files/usr/etc/motd
+mv bash.bashrc /data/data/com.termux/files/usr/etc
+mv lib /data/data/com.termux/files/usr/etc
+cd ..
+rm -rf BannerX
+echo
+clear
+echo
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        ✪Created by 'NAEEM KHAN'   ✪
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo -e '\033[1;96m' " |        ✪Instagram: naeemn2m          ✪
+echo -e '\033[1;96m' "══════════════════════════════════════════"
+echo
+figlet Finished | lolcat
+echo -e '\033[1;96m' "Restart your termux and see awesome look"
+echo
